69f502e892a80bbf03238019c19d001f45a1c273
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2022-09-21 10:31:35 +0200

    Update: GameView, Constants, main

---
+++ src/main/java/main/Constants.java
@@ -0,0 +1,9 @@
+package main;
+public class Constants {
+    public static final int SCREEN_HEIGHT = 400;
+    public static final int SCREEN_WIDTH = 800;
+    public static final String SCREEN_TITLE = "JumpNStuff";
+    public static final int RECT_WIDTH = 50;
+    public static final int RECT_HEIGHT = 50;
+}
+
--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,10 +1,12 @@
 package main;
+import view.GameView;
 
 public class main {
 
     public static void main(String args[]){
 
         System.out.println("Hello, do you wanna jump and stuff?");
+        GameView gameview = new GameView();
 
     }
 
