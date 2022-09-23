d81fae363b35ff653514a2a8d4a4065a8c1d91c1
elinruud <93664069+elinruud@users.noreply.github.com>
2022-09-23 13:39:40 +0200

    Gjort en ny klass med bakgrund som kopplar till GameView

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -7,11 +7,9 @@ import java.awt.*;
 public class GameView extends JFrame{
     public GameView() {
         DrawGameBackground dbg = new DrawGameBackground(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
-        /*ProgressBar pb = new ProgressBar(); */
         this.setTitle(Constants.SCREEN_TITLE);
         this.setSize(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
         this.add(dbg);
-        /*this.add(pb);*/
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
