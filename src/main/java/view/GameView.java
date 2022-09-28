7841c8463697a6b05becaf7dc9da5faf146b191f
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2022-09-28 14:18:56 +0200

    Ny GameView

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,24 +1,42 @@
 package view;
 import Utilities.Constants;
 
-
 import javax.swing.JFrame;
 import java.awt.*;
 
-
 public class GameView extends JFrame{
-    ProgressBar progressBar = new ProgressBar() ;
-
     public GameView() {
-        DrawGameBackground dbg = new DrawGameBackground(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
+        Thread animationThread = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    repaint();
+                    try {
+                        Thread.sleep(10);
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        });
+
+        animationThread.start();
+
+        // DrawRect drawrect = new DrawRect(Constants.RECT_HEIGHT, Constants.RECT_WIDTH);
+        //this.add(drawrect);
+        // DrawGameBackground dbg = new DrawGameBackground(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
         this.setTitle(Constants.SCREEN_TITLE);
         this.setSize(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
-        this.add(dbg);
+        // this.add(dbg);
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.add(new PaintingStuff());
+        //this.add(new DrawGameBackground(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT));
+        //this.add(new DisplayObjects());
+        //this.add(new Canvas());
+        //this.add(new DrawCirc(Constants.RECT_HEIGHT,Constants.RECT_WIDTH));
 
 
     }
 }
+
