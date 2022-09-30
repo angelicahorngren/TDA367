1606718c68cf002214e806a16ee8f164fcd8c109
jalalsankar <jalals@student.chalmers.se>
2022-09-30 12:17:06 +0200

    Add two new method to progressBar that are called in thread, make small changes to gameview so that we have better fuctional decomposition

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,26 +1,19 @@
 package view;
 import Utilities.Constants;
-
 import javax.swing.JFrame;
-import java.awt.*;
 
 public class GameView extends JFrame{
 
 
     public GameView() {
-
         ProgressBar progressBar = new ProgressBar();
         add(progressBar);
-        //double increase = progressBar.getIncreasedTime(Constants.SEC_LV1);
         Thread animationThread = new Thread(new Runnable() {
             public void run() {
-                 int counter = 0;
-                 double x = 0;
-
-                while (true || counter <= 100 ) {
+                while (true || progressBar.counter <= 100 ) {
                     paintComponents(getGraphics());
                     //repaint();
-                    progressBar.setValue(counter);
+                    progressBar.setValue(progressBar.counter);
                     progressBar.setBounds(200,10,400,10);
                     try {
                         Thread.sleep(10);
@@ -28,22 +21,10 @@ public class GameView extends JFrame{
                     } catch (Exception ex) {
                     }
 
-                    while (x % 1 != 0.0 || x == 0.0) {
-                        x = (x + progressBar.getIncreasedTime(Constants.SEC_LV1));
-                        x = Math.round(x * 1000000000d) / 1000000000d;
-                        //System.out.println(x);
-                        break;
-                    }
-                    if (x % 1 == 0.0) {
-
-                        counter += 1;
-                        x = 0;
+                    progressBar.updatetime();
+                    progressBar.increaseIfWholeNumber();
 
 
-
-
-                    }
-                    else continue;
                     }
                 }
 
--- src/main/java/view/ProgressBar.java
+++ src/main/java/view/ProgressBar.java
@@ -3,28 +3,23 @@ package view;
 import Utilities.Constants;
 
 import java.awt.*;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.util.Arrays;
 import javax.swing.*;
 
 
 public class ProgressBar extends JProgressBar {
 
-    boolean angelica =false;
-
-    public int counter = 0;
-
+    double x = 0;
+    int counter = 0;
 
     public ProgressBar() {
 
         this.setValue(0);
-        this.setBounds(10, 10, 100, 10);
+        this.setBounds(200, 2, 420, 20);
 
         this.setStringPainted(true); // show the percentage on the bar
         this.setFont(new Font("MV Boli", Font.BOLD, 10)); // customize the bar
         this.setForeground(Color.red); // set the fill color of the bar
-        this.setBackground(Color.BLACK); // set the background color of the bar
+        this.setBackground(Color.red); // set the background color of the bar
         UIManager.put("ProgressBar.selectionBackground", Color.red);
 
 
@@ -49,30 +44,21 @@ public class ProgressBar extends JProgressBar {
         return increase;
     }
 
-    public boolean timeUpdate() {
-        double x = 0;
-
+    public void updatetime() {
         while (x % 1 != 0.0 || x == 0.0) {
             x = (x + getIncreasedTime(Constants.SEC_LV1));
             x = Math.round(x * 1000000000d) / 1000000000d;
             //System.out.println(x);
             break;
         }
-        if (x % 1 != 0.0) {
-            return false;
-        } else return true;
+    }
+
+    public void increaseIfWholeNumber() {
+        if (x % 1 == 0.0) {
+
+            counter += 1;
+            x = 0;
+
+        }
     }
 }
-
-   /* public void makeAngelicafalse(){
-        angelica = false;
-    }
-
-
-    public void makeFalse () {
-
-
-    }
-
-
-}*/
