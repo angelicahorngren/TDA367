7f6d7cd3d92727b681d8b543e13f502baad3cf0c
Adam Boman <adamoboman@gmail.com>
2022-10-05 12:18:18 +0200

    Player class with ability to jump and fall with gravity

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,13 +1,18 @@
 package main;
 import view.GameView;
+import view.MenuView;
+
+import java.awt.*;
 
 public class main {
 
     public static void main(String args[]){
 
         System.out.println("Hello, do you wanna jump and stuff?");
-        GameView gameview = new GameView();
+        MenuView menuView = new MenuView();
+
 
     }
+
 
 }
--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,24 +1,30 @@
 package view;
+import Controller.MouseListener;
+import Model.Player;
 import Utilities.Constants;
 import javax.swing.JFrame;
 
 public class GameView extends JFrame{
 
+    MouseListener mouseListener;
 
     public GameView() {
         ProgressBar progressBar = new ProgressBar();
         add(progressBar);
+        mouseListener = new MouseListener();
+        addMouseListener(mouseListener);
         Thread animationThread = new Thread(new Runnable() {
             public void run() {
                 while (true) {
-                    //paintComponents(getGraphics());
+                    Player.gravity();
+                    if(mouseListener.mousePressed){
+                        Player.jump();
+                    }
                     repaint();
                     progressBar.setUpdatedCounter();
                     progressBar.setProgressbarBounds();
-
-
                     try {
-                        Thread.sleep(10);           //repaints the game view every 10 milliseconds
+                        Thread.sleep(15);           //repaints the game view every 10 milliseconds
 
                     } catch (Exception ex) {
                     }
