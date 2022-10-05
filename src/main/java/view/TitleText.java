29dd1115acbb2714bf531dcb63e9c6ea33cf580a
elinruud <93664069+elinruud@users.noreply.github.com>
2022-10-05 13:20:48 +0200

    Upptäckte direkt att texterna hade lite fler olika egenskaper än själva texten så la till x- och y-centering och textstorlek i konstruktorn.

--- src/main/java/view/TitleText.java
+++ src/main/java/view/TitleText.java
@@ -6,16 +6,22 @@ import java.awt.*;
 
 public class TitleText implements Texts {
 
-    public String text;
+    private String t;
+    private int s;
+    private int xc; //Placement of text
+    private int yc;
 
-    TitleText(String t){
-        text = t;
+    TitleText(String text, int size, int Xcentering, int Ycentering){
+        t = text;
+        s = size;
+        xc = Xcentering;
+        yc = Ycentering;
     }
     @Override
     public void draw(Graphics g){
-        Font titleFont = new Font("Bell MT", Font.BOLD, 30);
+        Font titleFont = new Font("Bell MT", Font.BOLD, s);
         g.setFont(titleFont);
         g.setColor(Color.BLACK);
-        g.drawString(text, Constants.LOST_ROUND_VIEW_WIDTH - 245, Constants.LOST_ROUND_VIEW_HEIGHT - 150) ;
+        g.drawString(t, xc, yc) ;
     }
 }
