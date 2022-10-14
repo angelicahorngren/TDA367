8a823805008ad0717d88dbd0e766e4a6d0900af6
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2022-10-14 10:30:33 +0200

    LevelOne class not finished

--- src/main/java/view/DrawCircView.java
+++ src/main/java/view/DrawCircView.java
@@ -6,22 +6,15 @@ import java.awt.*;
 
 public class DrawCircView extends JComponent {
 
-    //public int hight;
-   // public int width;
     Obstacle obstacle;
-
-    public DrawCircView(int h, int w) {
-        this.obstacle = new Obstacle(h, w, Constants.OBSTACLE_SPEED, Constants.OBSTACLE_START_X, Constants.OBSTACLE_START_Y);
-        //this.hight = obstacle.Height;
-        //this.width = obstacle.Width;
-
+    public DrawCircView(Obstacle obstacle) {
+        this.obstacle = obstacle;
 
     }
 
     public void draw3(Graphics g) {
 
         Graphics2D gg = (Graphics2D) g;
-
 
         gg.setColor(Constants.RECT_COLOR);
         gg.fillOval(obstacle.getxPosition(),Constants.Y_POS, obstacle.width, obstacle.height);      //här sätts även yposition
